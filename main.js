d49b76efa9668a9019ca050281a2a415d1b228c2
Benjamin Iniobong Clement <iniben007@gmail.com>
2023-04-10 17:12:40 +0100

    part home page fro tribal gateway website
    ##What has been done?
    - favicon created
    - links to external libraries like google fonts, bootstrap and icon library added
    - Responsive design styles added
    - and others
    
    ##Usage
    Open the index.html file in vs code

---
+++ main.js
@@ -0,0 +1,11 @@
+//function to toggle the navigation bar
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
+
+//end of function
