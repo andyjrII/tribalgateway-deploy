52fd5567f99dc0f663957c98fe4ad9063dde4b4d
Benjamin Iniobong Clement <iniben007@gmail.com>
2023-04-12 10:23:44 +0100

    Made adjustments for smaller screen sizes

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ menu.onclick = () => {
 };
 
 //end of function
+
+
+//Function to control the display of the navbar on small screens
+const showButton = document.querySelector('#menu-icon');
+const myElement = document.querySelector('.navbar');
+
+if (showButton && myElement) {
+  showButton.addEventListener('click', () => {
+    if (myElement.style.display === 'block') {
+      myElement.style.display = 'none';
+    } else {
+      myElement.style.display = 'block';
+    }
+  });
+} else {
+  console.error('Error: could not find one or more required elements.');
+}
